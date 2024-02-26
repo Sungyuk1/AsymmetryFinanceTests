fbc7a2b39310c534a0e5016512ca49356cb5d3da
Sungyuk1 <kwonsun7@msu.edu>
2024-02-26 01:09:53 -0500

    Added test for minting and withdrawing right away

--- SungyuTests/AfEthTests/AfEthMint.test.js
+++ SungyuTests/AfEthTests/AfEthMint.test.js
@@ -128,8 +128,9 @@ describe("Test AfEth", async function () {
         beforeEach(
             async () => await resetToBlock(parseInt(process.env.BLOCK_NUMBER ?? "0"))
         );
+          
 
-        it("Should mint", async function () {
+        it("Basic Mint", async function () {
             const depositAmount = ethers.utils.parseEther("1");
             await delay(300);
             const mintTx = await afEth.deposit(0, { value: depositAmount });
@@ -141,9 +142,36 @@ describe("Test AfEth", async function () {
                 accounts[0].address
               );
             await delay(300);
+            
+            console.log("This is the balance after minting with .1 ether : " + afEthBalanceBeforeRequest);
             expect(afEthBalanceBeforeRequest).gt(0);
-
         })
+
+        it("Mint and Withdrawl All", async function () {
+          const depositAmount = ethers.utils.parseEther("1");
+          await delay(300);
+          const mintTx = await afEth.deposit(0, { value: depositAmount });
+          await delay(300);
+          await mintTx.wait();
+          await delay(300);
+
+          const afEthBalanceBeforeRequest = await afEth.balanceOf(
+              accounts[0].address
+            );
+          await delay(300);
+          
+          console.log("This is the balance after minting with .1 ether : " + afEthBalanceBeforeRequest);
+          expect(afEthBalanceBeforeRequest).gt(0);
+
+          const requestWithdrawTx = await afEth.requestWithdraw(
+            await afEth.balanceOf(accounts[0].address)
+          );
+          await requestWithdrawTx.wait();
+      
+          const afEthBalanceAfterRequest = await afEth.balanceOf(accounts[0].address);
+          console.log("This is the balance after withdrawing the entire balance of account : "+afEthBalanceAfterRequest);
+          expect(afEthBalanceAfterRequest).eq(0);
+      })
 
 
 
